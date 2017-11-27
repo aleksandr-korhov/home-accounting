5236620820c6aaf5e989438b3b28b93a2db7468e
Aleksandr <akorhov@usetech.ru>
2017-11-27 23:28:37 +0300

    auth fix bugs

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
@@ -14,7 +13,6 @@ import { CommonModule } from '@angular/common';
     AuthComponent
   ],
   imports: [
-    AppRoutingModule,
     AuthRoutingModule,
     SharedModule,
     CommonModule
